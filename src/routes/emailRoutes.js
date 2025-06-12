3ec759826e08ee6428f06c85b5400bbd317fbaaf
Ravikant Tiwari <ravikanttiwari@aesl.in>
2025-06-12 21:08:14 +0530

    feat: added rate-limiting on email generate & rewrite route

--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const {authenticateToken} = require('../middlewares/authMiddleware')
 const {userEmailHistory, generateEmail, rewriteEmail, saveEmail} = require('../controllers/emailController')
+const {emailRateLimiter} = require('../middlewares/rateLimiter')
 const router = express.Router()
 
 router.get('/history', authenticateToken, userEmailHistory)
-router.post('/generate', authenticateToken, generateEmail)
-router.post('/rewrite', authenticateToken, rewriteEmail)
+router.post('/generate', emailRateLimiter, authenticateToken, generateEmail)
+router.post('/rewrite', emailRateLimiter, authenticateToken, rewriteEmail)
 router.post('/save', authenticateToken, saveEmail)
 
 module.exports = router
