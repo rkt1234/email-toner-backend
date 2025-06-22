3e243ad9f5f5aac84f718cbf751307e0ed817af6
Ravikant Tiwari <ravikanttiwari@aesl.in>
2025-06-23 00:03:28 +0530

    feat: implement rate limiter using environment variables

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -2,8 +2,8 @@
 const rateLimit = require('express-rate-limit');
 
 exports.emailRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 1 requests per windowMs
+  windowMs: process.env.WINDOW_WIDTH * 60 * 1000, // 15 minutes
+  max: process.env.EMAIL_SERVICE_LIMIT, // limit each IP to 1 requests per windowMs
   message: {
     status: 429,
     error: 'Too many requests, please try again later.',
